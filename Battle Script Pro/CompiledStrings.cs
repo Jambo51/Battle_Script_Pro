98c5666f6eda14f44535a8217a54e8ca66a22b8e
Jamie Christie <jambo51@hotmail.co.uk>
2013-06-15 20:37:00 +0100

    Added Licence

---
+++ Battle Script Pro/CompiledStrings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Battle_Script_Pro
+{
+    public class CompiledStrings
+    {
+        private int[] table;
+        private byte[] compiledStrings;
+
+        public int[] Table
+        {
+            get { return table; }
+            set { table = value; }
+        }
+
+        public byte[] CompiledStringsArray
+        {
+            get { return compiledStrings; }
+        }
+
+        public CompiledStrings(int[] theTable, byte[] theCompiledStrings)
+        {
+            table = theTable;
+            compiledStrings = theCompiledStrings;
+        }
+    }
+}
